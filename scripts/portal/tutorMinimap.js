2570240b4455f70bc9ff97843842a1ca191f4652
kevintjuh93 <kevintjuh93@59296cf3-ab24-4890-a14e-c0dede3ece5d>
2010-11-22 21:11:01 +0000

    Fixed Cygnus Tutorial (NOT FULLY BUT YOU CAN LEAVE IT NOW :3)
    git-svn-id: https://svn6.assembla.com/svn/MoopleDEV@83 59296cf3-ab24-4890-a14e-c0dede3ece5d

--- scripts/portal/tutorMinimap.js
+++ scripts/portal/tutorMinimap.js
@@ -1,7 +1,7 @@
 function enter(pi) {
 	pi.updateCygnusIntroState("minimap=clear;helper=clear");
 	pi.updateQuest(20022, "1");
-	pi.displayGuide(1);
+	pi.guideHint(1);
 	pi.blockPortal();
 	return true;
 }
